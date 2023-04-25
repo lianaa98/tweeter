afc98daf1ce45222e006b3c93eeefd84dd729f3d
lianaa98 <liana.kan98@gmail.com>
2023-04-24 21:05:35 -0700

    implemented characters left

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+// jQuery
+
+$(document).ready(() => {
+  console.log("This document is ready.");
+
+  /*  monitoring the input character length
+  if character length > 140, appear negative value */
+  function updateCharLength(charLength) {
+    const displayCount = 140 - charLength;
+    return displayCount;
+  }
+
+  $("#tweet-text").on("input", function() {
+    const charLimit =  updateCharLength(this.value.length);
+    const count = $(this).siblings("div").find(".counter");
+    count.text(charLimit);
+
+    if (charLimit < 0) {
+      count.css("color", "red");
+    }
+  });
+});
